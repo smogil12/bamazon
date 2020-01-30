86733fcf3279adac66608f642e636ee10f4d9b6b
Spencer Mogil <smogil12@gmail.com>
2020-01-30 18:38:57 -0500

    chore: fix gitignore and addmysql connection

---
+++ bamazonCustomer.js
@@ -0,0 +1,13 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  port: 3306,
+
+  user: "root",
+
+  password: "1234",
+  database: "bamazon"
+});
